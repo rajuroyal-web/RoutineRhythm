695ccc98fa344be3db352db2d105d030c65c7e55
kodipunjula yuvaraj <82932928+rajuroyal-web@users.noreply.github.com>
2024-11-11 18:42:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,30 +2,53 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
-const connectDB = require('./config/db');
+const DailyData = require('./models/DailyData');
 
 const app = express();
-
-// Connect to database
-connectDB();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-
-// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
 // API routes
-app.use('/api', require('./routes/api'));
+app.get('/api/data/:date', async (req, res) => {
+  try {
+    const data = await DailyData.findOne({ date: req.params.date });
+    res.json(data || {});
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.post('/api/data/:date', async (req, res) => {
+  try {
+    let data = await DailyData.findOne({ date: req.params.date });
+    if (data) {
+      data = await DailyData.findOneAndUpdate({ date: req.params.date }, req.body, { new: true });
+    } else {
+      data = new DailyData({ date: req.params.date, ...req.body });
+      await data.save();
+    }
+    res.json(data);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 // Serve index.html for all other routes
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-module.exports = app;
